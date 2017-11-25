f041a97746b4a8af009d442986d07da6f2711237
Lakshitha Herath <lakshitha@Lakshithas-MacBook-Pro.local>
2017-11-25 20:23:46 +0530

    new added

---
+++ SpringBoot/eureka-service-registry/src/main/java/com/mitrai/eurekaserviceregistry/EurekaServiceRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.mitrai.eurekaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
+	}
+}
