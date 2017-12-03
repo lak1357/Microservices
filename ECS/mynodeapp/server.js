757da3883e0964521b17f12f6b30430f04c1c73f
Lakshitha Herath <lakshitha@Lakshithas-MacBook-Pro.local>
2017-12-03 23:56:39 +0530

    new dev

--- ECS/mynodeapp/server.js
+++ ECS/mynodeapp/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello First Node APP\n');
+  res.send('Hello First Node APP and ECR.....\n');
 });
 
 app.listen(PORT, HOST);
