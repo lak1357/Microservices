81e555dba7381f361019f67179ed6e8f823ba708
Lakshitha Herath (543830003) <lakshitha.herath@moneta.cz>
2019-02-16 22:52:56 +0530

    order service added

--- micronaut/gateway/src/main/java/gateway/client/orderservice/OrderClientFallback.java
+++ micronaut/gateway/src/main/java/gateway/client/orderservice/OrderClientFallback.java
@@ -1,6 +1,8 @@
 package gateway.client.orderservice;
 
 import gateway.client.orderservice.model.Order;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Status;
 import io.micronaut.retry.annotation.Fallback;
 import io.reactivex.Single;
 
@@ -12,6 +14,7 @@ public class OrderClientFallback implements OrderOperation {
 
 
     @Override
+    @Status(HttpStatus.SERVICE_UNAVAILABLE)
     public Single<List<Order>> getOrders() {
         List<Order> orders = new ArrayList<>();
         return Single.just(orders);
